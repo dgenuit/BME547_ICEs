e952e5feac7b1ae4ce0a11981f230c514ec0f987
Drew Genuit <drewgenuit@Drews-MBP.lan>
2021-09-13 20:18:28 -0400

    Fixed some misspelled variables/functions and ensured PEP8 style

--- test_y_coordinate_calc.py
+++ test_y_coordinate_calc.py
@@ -1,6 +1,7 @@
 import pytest
 
-@pytest.mark.parmetrize("Point_1, Point2, new_x, expected_y_coord", [
+
+@pytest.mark.parametrize("Point_1, Point_2, new_x, expected_y_coord", [
     ((1, 1), (2, 2), 3, 3),
     ((2, 6), (10, 10), -12, -1),
     ((-1, 1), (4, -9), 0, -1)
